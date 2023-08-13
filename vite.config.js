5dc50beb50ab36b3f280c13f74f2b34c659467f6
lathan britz <lathan.britz@drupalconnect.com>
2023-08-13 11:20:07 -0400

    set port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [mkcert(), vue()],
+    server: {
+        strictPort: true,
+        port: 5173,
+      },
     define: {
         'process.env': {}
     }
